a6825a223da8c6bf707463da9dedceb99fa71070
Abdelffatteh Chebil <abdelffatteh.chebil@fullsix.com>
2017-04-06 18:18:28 +0200

    genarate views

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -8,8 +8,19 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class AdminController
+class AdminController extends Controller
 {
 
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin.html.twig');
+    }
 }
--- src/AppBundle/Controller/ReservationController.php
+++ src/AppBundle/Controller/ReservationController.php
@@ -14,8 +14,6 @@ class ReservationController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('reservation.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('reservation.html.twig');
     }
 }
